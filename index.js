4605ade82ec3a0e27f162008be9f9417c288ca91
ThalesFortes <thales_pro@outlook.com>
2025-03-28 22:44:01 -0300

    Criando novas tarefas

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ let tasks = [
   {id:3, description: "fazer o almoço", checked: false},
 ]
 
-const getCheckboxInput = ({id, description }) => {
+const createTaskListItem = (task, checkbox) =>{
+  const list = document.getElementById('todo-list');
+  const toDo = document.createElement('li')
+
+  toDo.id = task.id;
+  toDo.appendChild(checkbox)
+  list.appendChild(toDo);
+
+  return toDo;
+}
+
+const getCheckboxInput = ({id, description, checked }) => {
   const checkbox = document.createElement('input');
   const label = document.createElement('label');
   const wrapper = document.createElement('div');
@@ -12,7 +23,7 @@ const getCheckboxInput = ({id, description }) => {
 
   checkbox.type = "checkbox";
   checkbox.id = checkboxID;
-  checkbox.checked = tasks.checked;
+  checkbox.checked = checked || false;
 
   label.textContent = description;
   label.htmlFor = checkboxID;
@@ -25,7 +36,35 @@ const getCheckboxInput = ({id, description }) => {
   return wrapper;
 }
 
+const getNewTaskId = () =>{
+  const lastID = tasks[tasks.length -1]?.id
+  return lastID ? lastID + 1 : 1;
+}
+
+const getNewTaskData = (event) =>{
+  const description = event.target.elements.description.value;
+  const id = getNewTaskId();
+
+  return {description, id};
+}
+
+const createTask = (event) =>{
+  event.preventDefault();
+  const NewTaskData = getNewTaskData(event)
+  //const {id,description} = NewTaskData;
+
+  const checkbox = getCheckboxInput(NewTaskData)
+  createTaskListItem(NewTaskData, checkbox)
+
+  tasks = [
+    ...tasks, 
+    {id:NewTaskData.id, description:NewTaskData.description, checked:false}]
+}
+
 window.onload = function() {
+  const form = document.getElementById('create-todo-form');
+  form.addEventListener('submit', createTask)
+
   tasks.forEach((task) => {
     const checkbox = getCheckboxInput(task);
 
